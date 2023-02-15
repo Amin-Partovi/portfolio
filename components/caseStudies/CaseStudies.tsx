9ca8227f51c5b660766d41f459f3a05b599a1f92
Amin <amin.frontend@gmail.com>
2023-02-15 10:56:03 +0330

    add title for case studies part

--- components/caseStudies/CaseStudies.tsx
+++ components/caseStudies/CaseStudies.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import SampleWork from "../sampleWork/SampleWork";
 
+import SampleWork from "../sampleWork/SampleWork";
 import projects from "./projects";
+import texts from "@/texts/texts";
 
 import styles from "./case-studies.module.css";
 
 const CaseStudies: React.FC = () => {
   return (
     <section className={styles.container}>
-      {projects.map((project) => (
-        <SampleWork
-          key={project.id}
-          title={project.title}
-          src={project.photo}
-          description={project.description}
-        />
-      ))}
+      <h1 className={styles.title}>{texts.CASES}</h1>
+      <div className={styles.grid}>
+        {projects.map((project) => (
+          <SampleWork
+            key={project.id}
+            title={project.title}
+            src={project.photo}
+            description={project.description}
+          />
+        ))}
+      </div>
     </section>
   );
 };
--- texts/texts.ts
+++ texts/texts.ts
@@ -9,7 +9,8 @@ const texts = {
   ABOUT_ME:
     "I have been a Front-end Developer for about 5 years. Front-end development is constantly changing, and I have embraced this opportunity by learning new technologies and enjoying my work. The complexity of my work has strengthened my problem-solving skills, and I welcome taking on upcoming challenges. At the moment, my focus is on application development and I have a great deal of experience working with JavaScript and React. Being a part of an agile team is something I am passionate about, and I enjoy the teamwork that comes with it.",
   ABOUT_ME_TITLE: "About Me",
-  ITS_ME:"It's Me"
+  ITS_ME: "It's Me",
+  CASES: "Case Studies",
 };
 
 export default texts;
