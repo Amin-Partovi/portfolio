798384e266b435c57c7c16d93173ea6e454ddee9
Amin <amin.frontend@gmail.com>
2023-02-16 18:16:12 +0330

    change default font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Sarabun } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const font = Sarabun({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
