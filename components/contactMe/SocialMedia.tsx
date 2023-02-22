15efa535a14c8cbea258c32696d571e0d965c74a
Amin <amin.frontend@gmail.com>
2023-02-22 16:25:56 +0330

    add functionality to my social media

--- components/contactMe/SocialMedia.tsx
+++ components/contactMe/SocialMedia.tsx
@@ -1,21 +1,54 @@
-import Image from "next/image";
+import { toast } from "react-toastify";
 
 import Gmail from "public/social/gmail.svg";
 import LinkedIn from "public/social/linkedin.svg";
 import Github from "public/social/github.svg";
 import WhatsApp from "public/social/whatsapp.svg";
 import texts from "@/texts/texts";
-
-import styles from "./social-media.module.css";
 import SocialMediaIcon from "./SocialMediaIcon";
 
+import styles from "./social-media.module.css";
+
+const LINKEDIN = "https://www.linkedin.com/in/amin-partovi-823393b2/";
+const GITHUB = "https://github.com/Amin-Partovi";
+const GMAIL = "amin.frontend@gmail.com";
+const WHATS_APP = "+989353200380";
+
 const SocialMedia: React.FC = () => {
+  function handleClickGmail() {
+    navigator.clipboard.writeText(GMAIL);
+    toast("🔥 now, you have my Gmail address in your clipboard", {
+      className: styles.toast,
+    });
+  }
+
+  function handleClickWhatsApp() {
+    navigator.clipboard.writeText(WHATS_APP);
+    toast("🔥 now, you have my phone number in your clipboard", {
+      className: styles.toast,
+    });
+  }
   return (
     <div className={styles["social-media"]}>
-      <SocialMediaIcon icon={Gmail} title={texts.GMAIL} />
-      <SocialMediaIcon icon={LinkedIn} title={texts.LINKEDIN} />
-      <SocialMediaIcon icon={Github} title={texts.GITHUB} />
-      <SocialMediaIcon icon={WhatsApp} title={texts.WHATSAPP} />
+      <SocialMediaIcon
+        icon={Gmail}
+        title={texts.GMAIL}
+        onClick={handleClickGmail}
+      />
+
+      <a href={LINKEDIN} target="_blank" rel="noreferrer">
+        <SocialMediaIcon icon={LinkedIn} title={texts.LINKEDIN} />
+      </a>
+
+      <a href={GITHUB} target="_blank" rel="noreferrer">
+        <SocialMediaIcon icon={Github} title={texts.GITHUB} />
+      </a>
+
+      <SocialMediaIcon
+        icon={WhatsApp}
+        title={texts.WHATSAPP}
+        onClick={handleClickWhatsApp}
+      />
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import { Sarabun } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const font = Sarabun({
   subsets: ["latin"],
@@ -11,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={font.className}>
       <Component {...pageProps} />
+      <ToastContainer />
     </main>
   );
 }
