60a968c9d7f72e89e0285e56f4e95a480d17bebf
Amin <amin.frontend@gmail.com>
2023-03-05 23:38:21 +0330

    add new article

--- components/publications/articles.ts
+++ components/publications/articles.ts
@@ -1,12 +1,12 @@
 const articles = [
   {
-    title: "Astro, Super Fast JavaScript Framework",
+    title: "How Browsers Make Websites Come to Life",
     description:
-      "This article introduces you to Astro JS, how it can be useful, where its idea comes from, how the code structure looks like, and how it is improved over the previous version.",
+      "This article explains how web browsers work and how they bring websites to life. It covers key concepts such as the critical rendering path, how a browser retrieves web page resources, and the sequence of steps a browser takes to convert HTML, CSS, and JavaScript into pixels on the screen. ",
     image:
-      "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*XCNmiduRLvC3N9Mev3HIbQ.png",
-    url: "https://medium.com/@amin.frontend/astro-super-fast-javascript-framwork-243b4b60c971",
-    publication: "",
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*j3sL4x_xDDGUTdDxjBQoDg.png",
+    url: "https://medium.com/geekculture/how-browsers-make-websites-come-to-life-1d748702f2e6",
+    publication: "Geek Culture",
   },
   {
     title: "Easily integrate multiple JavaScript frameworks into one web page",
@@ -16,6 +16,16 @@ const articles = [
       "https://miro.medium.com/v2/resize:fit:828/format:webp/1*C5Rtvn_UnC7Q8ZWZ6LBc1w.jpeg",
     url: "https://medium.com/geekculture/easily-integrate-multiple-javascript-frameworks-into-one-web-page-8a785ec3a74c",
     publication: "Geek Culture",
+  },
+
+  {
+    title: "Astro, Super Fast JavaScript Framework",
+    description:
+      "This article introduces you to Astro JS, how it can be useful, where its idea comes from, how the code structure looks like, and how it is improved over the previous version.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*XCNmiduRLvC3N9Mev3HIbQ.png",
+    url: "https://medium.com/@amin.frontend/astro-super-fast-javascript-framwork-243b4b60c971",
+    publication: "",
   },
 ];
 
